ab2a3ac69454ff5a70a549a805717dd2ad407bda
Дмитрий <rabolan@mail.ru>
2019-08-07 23:27:33 +0300

    add datagrid

--- VirusServer/Form1.Designer.cs
+++ VirusServer/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ClientTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTable)).BeginInit();
             this.SuspendLayout();
+            // 
+            // ClientTable
+            // 
+            this.ClientTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientTable.Location = new System.Drawing.Point(12, 12);
+            this.ClientTable.Name = "ClientTable";
+            this.ClientTable.Size = new System.Drawing.Size(542, 179);
+            this.ClientTable.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 161);
+            this.ClientSize = new System.Drawing.Size(566, 215);
+            this.Controls.Add(this.ClientTable);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTable)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView ClientTable;
     }
 }
 
--- VirusServer/Form1.cs
+++ VirusServer/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace VirusServer
 {
+   
     public partial class Form1 : Form
     {
+        public Thread refreshThread; // отдельный поток
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //при загрузке формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshThread = new Thread(WaitForConnect);
+            refreshThread.Start();// запуск метода в отдельном потоке
+        }
+
+        public static void WaitForConnect()
+        {
+
+        }
+
+        //после закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            refreshThread.Abort();// завершение потока
         }
     }
 }
