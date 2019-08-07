ee227673ed9997b70837189d2b3b8d5df8b9c710
Дмитрий <rabolan@mail.ru>
2019-08-07 12:51:39 +0300

    рабочий фон

---
+++ WpfTestPone/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestPone
+{
+    class ApplicationContext : DbContext
+    {
+        /// <summary>
+        /// Связь между программой и БД
+        /// </summary>
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Phone> Phones { get; set; }
+
+    }
+}
