6f3c2598f3796a33feec2fef05c888533311c93b
Дмитрий <rabolan@mail.ru>
2019-08-12 21:07:34 +0300

    добавлен Тестовой проект виндовс форм

---
+++ WindowsFormsTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //Кнопка выхода
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Кнопка отправить
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Кнопка загрузить картинку
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
