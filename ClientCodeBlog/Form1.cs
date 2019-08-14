4eb526f933ca01bca1af0c01f31bf5958dd0ab4b
Дмитрий <rabolan@mail.ru>
2019-08-14 16:11:55 +0300

    рабочая версия

--- ClientCodeBlog/Form1.cs
+++ ClientCodeBlog/Form1.cs
@@ -15,8 +15,9 @@ namespace ClientCodeBlog
     public partial class Form1 : Form
     {
         //Переменные
-        const string ip = "192.168.1.78"; // д 192.168.0.45
+        const string ip = "192.168.1.54"; // д 192.168.0.45 192.168.1.54
         const int port = 19132;
+        string message="";
 
         IPEndPoint tcpEndPoint;
         Socket tcpSocket;
@@ -34,26 +35,33 @@ namespace ClientCodeBlog
         //кнопка выход
         private void Button1_Click(object sender, EventArgs e)
         {
-
+            tcpSocket.Shutdown(SocketShutdown.Both); //двухстроние закрытие соединение. на снервере и на клиенте
+            tcpSocket.Close();
+            Close();
         }
 
         //Кнопка отправить
         private void Button2_Click(object sender, EventArgs e)
         {
-
+            string dateTime = DateTime.Now.ToString();
+             message += $"{dateTime}\t\nСообщение от клиента: {textBox1.Text}"; //  тело сообщенияя
+            // await Task.Run(() => InicializatorClienta());
+            InicializatorClienta();
+            // message += textBox1.Text;
+            // Invoke((MethodInvoker)delegate { label1.Text = log; ; }); //вывод в лабел минуя ошибку
         }
 
         //При загрузке формы
         private async void Form1_Load(object sender, EventArgs e)
         {
-            await Task.Run(() => InicializatorClienta());
+          //  await Task.Run(() => InicializatorClienta());
         }
 
         //Методы
         public void InicializatorClienta()
         {
-            string dateTime = DateTime.Now.ToString();
-            string message = $"{dateTime}\t\nСообщение:{textBox1.Text}"; //  тело сообщенияя
+            
+           // string message = $"{dateTime}\t\nСообщение:{textBox1.Text}"; //  тело сообщенияя
 
             var data = Encoding.UTF8.GetBytes(message); // кодировка сообщение в байты. переод отправкой серверу
 
@@ -61,7 +69,23 @@ namespace ClientCodeBlog
             tcpSocket.Connect(tcpEndPoint);
             tcpSocket.Send(data); //отправка сообщения
 
+            var buffer = new byte[256]; // массив буфера
+            var size = 0;
+            var answer = new StringBuilder(); //
 
+            do // проверяем условие о получили запрос
+            {
+                size = tcpSocket.Receive(buffer); // полученно все байти сообщения. Маскимальное  
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size)); // добавляем полученые через сокет  раскадированное данные из бкфера
+                log += "Полeчены данные от клиента \t\n";
+            }
+            while (tcpSocket.Available > 0); // если полученные данные еще есть. цикл работает
+            log += answer.ToString();  // сохраняем полученное сообщение
+
+            Invoke((MethodInvoker)delegate { label1.Text = log; ; }); //вывод в лабел минуя ошибку 
+
+           // tcpSocket.Shutdown(SocketShutdown.Both); //двухстроние закрытие соединение. на снервере и на клиенте
+           // tcpSocket.Close();
         }
     }
 }
--- ServerCodeBlog/Form1.cs
+++ ServerCodeBlog/Form1.cs
@@ -18,11 +18,12 @@ namespace ServerCodeBlog
     public partial class Form1 : Form
     {
         //Переменные
-        const string ip = "192.168.1.78"; // д 192.168.0.45
+        const string ip = "192.168.1.54"; // д 192.168.0.45
         const int port = 19132;
 
         IPEndPoint tcpEndPoint;
         Socket tcpSocket;
+        Socket listener;
 
         string log = "Журнал событий \t\n";
 
@@ -34,11 +35,7 @@ namespace ServerCodeBlog
             InitializeComponent();
         }
 
-        //Кнопка выход
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+      
 
         //При загрузке формы
         private async void Form1_Load(object sender, EventArgs e)
@@ -62,7 +59,7 @@ namespace ServerCodeBlog
 
             while (true) //TODO можно реализовывать проверку спомощью булевой переменной
             {
-                var listener = tcpSocket.Accept(); // прошлушка сокета и при подключении создание нового скета для ответа
+                listener = tcpSocket.Accept(); // прошлушка сокета и при подключении создание нового скета для ответа
                 var buffer = new byte[256]; // массив буфера
                 var size = 0;
                 var data = new StringBuilder();
@@ -73,24 +70,36 @@ namespace ServerCodeBlog
                 {
                     size = listener.Receive(buffer); // полученно все байти сообщения. Маскимальное  
                     data.Append(Encoding.UTF8.GetString(buffer, 0, size)); // добавляем полученые через сокет  раскадированное данные из бкфера
-                    log += "Полечены данные от клиента \t\n";
+                    log += "Получены данные от клиента \t\n";
                 }
                 while (listener.Available > 0); // если полученные данные еще есть. цикл работает
 
                 Invoke((MethodInvoker)delegate { label1.Text = data.ToString(); ; }); // загружаем в таблицу данные полученнфые из листа
 
-                label1.Text = data.ToString(); //выводим полученную инфу
+               // label1.Text = data.ToString(); //выводим полученную инфу
 
                 //Текущие сообщение сообщает об успешном соединении. В принципе можно отправлять и другие сообщения
-                listener.Send(Encoding.UTF8.GetBytes("Успех")); //отправка сообщение через сокет. 
+                listener.Send(Encoding.UTF8.GetBytes("Успех")); //отправка сообщение через сокет что связь установленная. 
 
                 //двухстроние закрытие соединение. на снервере и на клиенте
-                listener.Shutdown(SocketShutdown.Both);
-                listener.Close(); //физическое закрытие обьекта 
-                log += "Закрытие соединение \t\n";
-                Invoke((MethodInvoker)delegate { label1.Text = log; ; }); //вывод в лабел минуя ошибку 
+                //listener.Shutdown(SocketShutdown.Both);
+                //listener.Close(); //физическое закрытие обьекта 
+                //log += "Закрытие соединение \t\n";
+                //Invoke((MethodInvoker)delegate { label1.Text += log; ; }); //вывод в лабел минуя ошибку 
+               //Invoke((MethodInvoker)delegate { label1.Text += log; ; }); //вывод в лабел минуя ошибку 
 
             }
+
+             
+        }
+
+        //Кнопка выход
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listener.Shutdown(SocketShutdown.Both);
+            listener.Close(); //физическое закрытие обьекта 
+            log += "Закрытие соединение \t\n";
+            Close();
         }
     }
 }
